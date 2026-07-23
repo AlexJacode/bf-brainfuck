afee2bc6e6a1575448ab01ef11b0acf55b1692f3
Alex <108822756+AlexJacode@users.noreply.github.com>
2026-07-23 02:53:25 -0400

    Create boyfriend.cpp
    wowza

---
+++ boyfriend.cpp
@@ -0,0 +1,63 @@
+// brainfuck == bf == boyfriend 
+
+#include <iostream> 
+#include <cstdint> 
+#include <cstdlib> 
+
+
+int bf(bool liv, std::string input, short index = 0, bool inner=0) { 
+    uint8_t arr[30000]; 
+    while (1) { 
+        if (liv == 0) { std::cout << "Input: "; }  
+        std::cin >> input; 
+        std::cout << "\n"; 
+        if (input==">") {  
+            ++index;
+        } 
+        else if (input== "<") {  
+            --index;
+        }
+        else if (input== "+") {  
+            arr[index]++; 
+        } 
+        else if (input=="-") { 
+            arr[index]--; 
+        }
+        else if (input==".") {  
+            std::cout << arr[index]; 
+        } 
+        else if (input==",") {          
+            uint8_t n; 
+            std::string n2; 
+            std::cout << "Input At " << index << ": "; 
+            std::cin >> n2; 
+            n = std::stoi(n2); 
+        } 
+        else if (input=="[") {  
+            std::string nin; 
+            std::cout << "Inner Input: "; 
+            std::cin >> nin; 
+            while (nin != "]") { 
+                if (liv==1) { bf(-1, nin, index=index); break; } 
+                bf(liv, input, index=index, inner=1);
+                std::string rin;
+                std::cout<<"\n Inner Input: "; 
+                std::cin>>rin;nin=rin; 
+            } 
+        } 
+        std::cout << "\n"; 
+    } 
+    return 0; 
+} 
+
+
+int main(int argc, char *argv[]) { 
+    std::cout << "Beginning Boyfriend Brainfuck \n"; 
+    if (argc>1) { bf(1, ""); return 0; } 
+    bf(0, ""); 
+    return 0; 
+} 
+
+// liv=0 == NON LIVE 
+// liv=1 == LIVE 
+// liv=-1 == LIVE INNER SCOPE 
